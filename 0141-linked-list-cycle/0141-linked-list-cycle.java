a9e57516ebf490283b3c3633f021c011ab2fa504
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-16 23:24:21 +0200

    Time: 0 ms (100%), Space: 45 MB (6.69%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode tor = head;
+        ListNode rab = head;
+        while(rab != null && rab.next != null){
+            tor = tor.next;
+            rab = rab.next.next;
+            if(rab == null){
+                return false;
+            }
+            else if(tor == rab){
+                return true;
+            }
+        }
+        return false;
+    }
+}
