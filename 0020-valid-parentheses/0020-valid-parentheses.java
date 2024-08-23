bc589fd652c59b109a40b0630b5f58153b24a4dd
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-24 00:09:04 +0200

    Time: 2 ms (52.05%), Space: 41.3 MB (67.8%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stos = new Stack<>();
+        if(s.length()%2 == 0 && s.length() != 0){
+            stos.push(s.charAt(0));
+            for(int i=1; i<s.length(); i++){
+                switch(s.charAt(i)){
+                    case ')':
+                        if(!stos.empty() && (int)stos.peek() == (int)s.charAt(i) - 1){
+                            stos.pop();
+                            break;
+                        }
+                        else{
+                            stos.push(s.charAt(i));
+                            break;
+                        }
+                    case ']':
+                        if(!stos.empty() && (int)stos.peek() == (int)s.charAt(i) - 2){
+                            stos.pop();
+                            break;
+                        }
+                        else{
+                            stos.push(s.charAt(i));
+                            break;
+                        }
+                    case '}':
+                        if(!stos.empty() && (int)stos.peek() == (int)s.charAt(i) - 2){
+                            stos.pop();
+                            break;
+                        }
+                        else{
+                            stos.push(s.charAt(i));
+                            break;
+                        }
+                    default:
+                        stos.push(s.charAt(i));
+                        break;
+                }
+            }
+            if(stos.empty()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+}
