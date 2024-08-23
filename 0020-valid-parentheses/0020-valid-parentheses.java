faefdcd9fff1245d85694dbfd3cbdbed8d0f2443
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-24 00:16:07 +0200

    Time: 2 ms (52.05%), Space: 41.5 MB (34.38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,37 +4,42 @@ class Solution {
         if(s.length()%2 == 0){
             stos.push(s.charAt(0));
             for(int i=1; i<s.length(); i++){
-                switch(s.charAt(i)){
-                    case ')':
-                        if(!stos.empty() && (int)stos.peek() == (int)s.charAt(i) - 1){
-                            stos.pop();
-                            break;
-                        }
-                        else{
+                if(!stos.empty()){
+                    switch(s.charAt(i)){
+                        case ')':
+                            if((int)stos.peek() == (int)s.charAt(i) - 1){
+                                stos.pop();
+                                break;
+                            }
+                            else{
+                                stos.push(s.charAt(i));
+                                break;
+                            }
+                        case ']':
+                            if((int)stos.peek() == (int)s.charAt(i) - 2){
+                                stos.pop();
+                                break;
+                            }
+                            else{
+                                stos.push(s.charAt(i));
+                                break;
+                            }
+                        case '}':
+                            if((int)stos.peek() == (int)s.charAt(i) - 2){
+                                stos.pop();
+                                break;
+                            }
+                            else{
+                                stos.push(s.charAt(i));
+                                break;
+                            }
+                        default:
                             stos.push(s.charAt(i));
                             break;
-                        }
-                    case ']':
-                        if(!stos.empty() && (int)stos.peek() == (int)s.charAt(i) - 2){
-                            stos.pop();
-                            break;
-                        }
-                        else{
-                            stos.push(s.charAt(i));
-                            break;
-                        }
-                    case '}':
-                        if(!stos.empty() && (int)stos.peek() == (int)s.charAt(i) - 2){
-                            stos.pop();
-                            break;
-                        }
-                        else{
-                            stos.push(s.charAt(i));
-                            break;
-                        }
-                    default:
-                        stos.push(s.charAt(i));
-                        break;
+                    }
+                }
+                else{
+                    stos.push(s.charAt(i));
                 }
             }
             if(stos.empty()){
