ef5f5694195fa03152b1bff02bf10fe1648d0d67
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-25 00:06:01 +0200

    Time: 14 ms (31.13%), Space: 45 MB (42.13%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,30 @@
+class Solution {
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] mag = magazine.toCharArray();
+        char[] rans = ransomNote.toCharArray();
+        for(int i=0; i<mag.length; i++){
+            if(this.map.containsKey(mag[i])){
+                this.map.put(mag[i], map.get(mag[i])+1);
+            }
+            else{
+                this.map.put(mag[i], 1);
+            }
+        }
+        for(int i=0; i<rans.length; i++){
+            if(this.map.containsKey(rans[i])){
+                if(this.map.get(rans[i])<1){
+                    return false;
+                }
+                else{
+                    this.map.put(rans[i], this.map.get(rans[i])-1);
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
