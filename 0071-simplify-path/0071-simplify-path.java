91fc7fb24774359b1fa89e331584dda529239031
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-24 02:37:00 +0200

    Time: 8 ms (24.28%), Space: 44.6 MB (15.1%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] tab = path.split("/");
+        Stack<String> stos = new Stack<>();
+        for(String element : tab){
+            switch(element){
+                case ".":
+                    continue;
+                case "..":
+                    if(!stos.empty()){
+                        stos.pop();
+                        break;
+                    }
+                    else{
+                        continue;
+                    }
+                case "":
+                    continue;
+                case "...":
+                    stos.push(element);
+                    break;
+                default:
+                    stos.push(element);
+                    break;
+            }
+        }
+        String new_path = new String();
+        while(!stos.empty()){
+            new_path = "/" + stos.pop() + new_path;
+        }
+        if(new_path.equals("")){
+            return "/";
+        }
+        else{
+            return new_path;
+        }
+    }
+}
