f3293f3093ad6c79960e2d9e5e0c843fdb35fdc8
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-25 22:48:17 +0200

    Time: 111 ms (15.91%), Space: 13 MB (73.83%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        i, j, k = 0, 1, 0
+        i, j = 0, 1
         while(j<len(nums)):
             if nums[i] == nums[j]:
                 del nums[j]
-                k+=1
             else:
                 i+=1
                 j+=1
-        nums += k*"_"
         return j
