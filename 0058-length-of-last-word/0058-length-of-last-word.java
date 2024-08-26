8195489e189e2c0f14d409f4658c54457e41d4b8
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-26 17:56:23 +0200

    Time: 0 ms (100%), Space: 41.9 MB (19.62%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -2,15 +2,16 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int i = s.length()-1;
         int j = s.length()-1;
-        while(i>0){
+        while(i>=0){
             if(!Character.isLetterOrDigit(s.charAt(i))){
                 i--;
                 j--;
             }
             else{
-                while(Character.isLetterOrDigit(s.charAt(i))){
+                while(i>0 && Character.isLetterOrDigit(s.charAt(i-1))){
                     i--;
                 }
+                i--;
                 break;
             }
         }
