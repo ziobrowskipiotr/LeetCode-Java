36a09e111b19537b49e1ee6b84cb759c8137e8b9
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-26 18:52:17 +0200

    Time: 5 ms (79.19%), Space: 41.5 MB (66.79%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,6 +1,5 @@
-//
 class Solution {
-    List<String> lista = new LinkedList<>();
+    List<String> lista = new ArrayList<>();
     public List<String> summaryRanges(int[] nums) {
         int i = 0;
         int j = 1;
@@ -10,14 +9,19 @@ class Solution {
             }
             else{
                 if(nums[i] != nums[j-1]){
-                    this.lista.add(""+nums[i]+"->"+nums[j-1]);
+                    this.lista.add(nums[i]+"->"+nums[j-1]);
                     i = j;
                     j++;
                 }
                 else{
-                    this.lista.add(""+nums[i]);
-                    i++;
-                    j++;
+                    this.lista.add(Integer.toString(nums[i]));
+                    if(i<nums.length){
+                        i++;
+                        j++;
+                    }
+                    else{
+                        break;
+                    }
                 }
             }
         }
