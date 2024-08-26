5453546edd909354741ec15d572583108600b06b
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-26 16:48:29 +0200

    Time: 1 ms (92.71%), Space: 41.8 MB (9.99%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while(i<s.length()){
+            while(j<t.length() && s.charAt(i) != t.charAt(j)){
+                j++;
+            }
+            if(j<t.length()){
+                i++;
+                j++;
+            }
+            else{
+                break;
+            }
+        }
+        if(i == s.length()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
