5ed7a1665352f7f7278b0ee0c365a77fb131f9a7
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-15 03:16:27 +0200

    Time: 0 ms (100%), Space: 41.1 MB (9.29%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,20 +1,13 @@
 class Solution {
     
     public int hammingWeight(int n) {
-        int[] tab = new int[]{1<<30, 1<<15, 0};
-        while(tab[1] > 0){
-            if((tab[1] & n)>0){
-                tab[2]++;
-            }
-            if(tab[0] < (1<<16)){
-                break;
-            }
+        int[] tab = new int[]{1<<30, 0};
+        while(tab[0] > 0){
             if((tab[0] & n)>0){
-                tab[2]++;
+                tab[1]++;
             }
             tab[0] >>=1;
-            tab[1] >>=1;
         }
-        return tab[2];
+        return tab[1];
     }
 }
