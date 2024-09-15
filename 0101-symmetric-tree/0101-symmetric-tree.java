30329bb96e9f2f55a513bf4b3e3bdd89e31f31e5
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-15 23:02:03 +0200

    Time: 0 ms (100%), Space: 41.8 MB (40.62%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+
     public boolean equal(TreeNode root1, TreeNode root2){
         if(root1 == null && root2 == null) return true;
         else if(root1 == null || root2 == null) return false;
