8c1923af165087526aad4360d278740c310490cd
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-17 01:41:05 +0200

    Time: 3 ms (26.93%), Space: 45.5 MB (64.37%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> lista = new ArrayList<>();
+        if(root == null){
+            return lista;
+        }
+        Deque<TreeNode> kolejka = new LinkedList<>();
+        kolejka.add(root);
+        int siz = 0;
+        double sum = 0;
+        while(!kolejka.isEmpty()){
+            siz = kolejka.size();
+            sum = 0;
+            for(int i=0; i<siz; i++){
+                sum+=kolejka.peekFirst().val;
+                if(kolejka.peekFirst().left != null){
+                    kolejka.add(kolejka.peekFirst().left);
+                }
+                if(kolejka.peekFirst().right != null){
+                    kolejka.add(kolejka.peekFirst().right);
+                }
+                kolejka.pollFirst();
+            }
+            lista.add(sum/siz);
+        }
+        return lista;
+    }
+}
