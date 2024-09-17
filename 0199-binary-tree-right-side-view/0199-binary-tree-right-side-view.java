ddd72b0834d8dc424f57f5dadeffb46938c6ba03
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-17 02:23:30 +0200

    Time: 1 ms (65.41%), Space: 42.1 MB (46.76%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -19,7 +19,7 @@ class Solution {
         if(root == null){
             return lista;
         }
-        Deque<TreeNode> kolejka = new ArrayDeque<>();
+        Deque<TreeNode> kolejka = new LinkedList<>();
         TreeNode cur;
         int siz;
         kolejka.add(root);
